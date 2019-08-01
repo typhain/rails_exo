62c1f6b6efa8268a0721b7aa663d2b3dc6b90c77
typhain <typhaine.charrat@gmail.com>
2019-08-01 16:43:30 +0200

    exo1_finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 require 'faker'
-# 50.times do |index|
-#   Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: ["allergologue", "anesthésiste", "cardiologue", "chirurgien", "généraliste", "dermatologiste", "endocrinologue"].sample,  zip_code: Faker::Address.zip_code)
+50.times do
+  Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Address.zip_code)
+end
+
+# 50.times do
+#   Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 # end
 
-# 50.times do |index|
-#   Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+# 50.times do
+#   Appointment.create(date: Faker::Date.forward(days: 30), doctor_id: rand(1..50), patient_id: rand(1..50))
 # end
 
-50.times do |index|
-  Appointment.create!(date: Faker::Date.forward(days: 30), doctor_id: rand(1..50), patient_id: rand(1..50))
+50.times do
+  Specialty.create(name: ["allergologue", "anesthésiste", "cardiologue", "chirurgien", "généraliste", "dermatologiste", "endocrinologue"].sample)
+end
+
+50.times do
+  JoinTableSpecialtyDoctor.create(doctor: Doctor.all.sample, specialty: Specialty.all.sample)
 end
