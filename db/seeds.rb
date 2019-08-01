8b3d5be0c1b54f789bb6781ad4b24ad84ef2297c
typhain <typhaine.charrat@gmail.com>
2019-08-01 19:48:08 +0200

    update_seeds_appointments

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,16 @@ end
 
 # 50.times do
 #   Appointment.create(date: Faker::Date.forward(days: 30), doctor_id: rand(1..50), patient_id: rand(1..50))
-# end
+# end()
+
+50.times do
+p  Appointment.create(date: Faker::Date.forward(days: 30))
+end
 
 50.times do
   Specialty.create(name: ["allergologue", "anesthésiste", "cardiologue", "chirurgien", "généraliste", "dermatologiste", "endocrinologue"].sample)
 end
+
 
 50.times do
   JoinTableSpecialtyDoctor.create(doctor: Doctor.all.sample, specialty: Specialty.all.sample)
